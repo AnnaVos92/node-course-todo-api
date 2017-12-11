a213b76a38cd3fd4d2447d0bef725a3e5040fa1f
Anna Vos <anna-h.vos@hotmail.com>
2017-12-11 12:10:07 +0100

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -25,10 +26,27 @@ app.post('/todos', (req, res) => {
   });
 });
 
+// get all todos
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos})   // pass as object instead of array for possibility to modify later
   }).catch((e) => res.status(400).send(e));
+});
+
+// get todo by passing id in link
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  };
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    };
+
+    res.send({todo})
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(3000, () => {
