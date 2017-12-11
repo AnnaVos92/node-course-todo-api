ff565cb782e4d87cd6454f352c7d39a42dcc365d
Anna Vos <anna-h.vos@hotmail.com>
2017-12-11 15:58:15 +0100

    test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -79,24 +79,62 @@ describe('GET /todos', () => {
 describe('GET /todos/:id', () => {
   it('should return todo doc', (done) => {
     request(app)
-    .get(`/todos/${todos[0]._id.toHexString()}`)
-    .expect(200)
-    .expect((res) => {
-      expect(res.body.todo.text).toBe(todos[0].text);
-    })
-    .end(done);
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
   });
 
   it('should return 404 if todo not found', (done) => {
     request(app)
-    .get(`/todos/${new ObjectID().toHexString()}`)
-    .expect(404)
-    .end(done);
+      .get(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
   });
 
   it('should return 404 for invalid ObjectIDs', (done) => {
     request(app)
-    .get('/todos/123')
+      .get('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexID = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexID);
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        };
+
+        // trying to delete a second time, we expect todo to not exist
+        Todo.findById(hexID).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+  
+  it('should return 404 if object ID is invalid', (done) => {
+    request(app)
+    .delete('/todos/123')
     .expect(404)
     .end(done);
   });
